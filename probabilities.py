f26d78cc6b2ab149dab3fbbb9bd6d4be736c4477
Sheden Andemicael <shedenandemicael@DNa1de864.SUNet>
2023-03-17 05:41:32 -0700

    calculated some bracket probabilities

--- probabilities.py
+++ probabilities.py
@@ -1,5 +1,7 @@
 import numpy as np
+import random
 from scipy.stats import norm
+from teams import first_round, first_round_id
 
 NUM_TEAMS = 363
 
@@ -11,6 +13,10 @@ def main():
 
     team1 = generate_dist(data[0])
     team2 = generate_dist(data[6])
+
+    print(highest_likelihood(first_round, data))
+    print(highest_ranked(first_round, data))
+    print(random_likelihood(first_round, data))
 
 def tournament_teams(data):
     """ 
@@ -56,6 +62,198 @@ def A_beats_B(team1, team2):
 
     # generate probability
     return norm.cdf(z)
+
+def simulate_game_highest_likelihood(next_round, i, game1_prob, game2_prob):
+    """
+    Simulates a game in the tournament bracket
+    where the team with the highest probability
+    of winning wins.
+    """
+    next_game = []
+    if game1_prob >= 0.5:
+        next_game.append(first_round_id[i][0])
+    else:
+        game1_prob = 1 - game1_prob
+        next_game.append(first_round_id[i][1])
+    
+    if game2_prob >= 0.5:
+        next_game.append(first_round_id[i + 1][0])
+    else:
+        game2_prob = 1 - game2_prob
+        next_game.append(first_round_id[i + 1][1])
+    # set up next round
+    next_round.append(next_game)
+
+def simulate_round_highest_likelihood(cur_round, next_round, bracket_likelihood, data):
+    """
+    The function generates a round and returns the
+    likelihood that the better team wins in each match
+    wins.
+    """
+    for i in range(0, len(cur_round), 2):
+        # generate probabilites
+        game1_prob = A_beats_B(generate_dist(data[first_round_id[i][0]]), generate_dist(data[first_round_id[i][1]]))
+        game2_prob = A_beats_B(generate_dist(data[first_round_id[i + 1][0]]), generate_dist(data[first_round_id[i + 1][1]]))
+
+        # set up next round
+        simulate_game_highest_likelihood(next_round, i, game1_prob, game2_prob)
+
+        # calculate current likelihood
+        bracket_likelihood *= game1_prob / 0.5 * game2_prob / 0.5
+    return bracket_likelihood
+
+def highest_likelihood(first_round, data):
+    """
+    Given a tournament bracket, the function
+    determines the likelihood of the most
+    probable bracket.
+    """
+    second_round = []
+    sweet_sixteen = []
+    elite_eight = []
+    final_four = []
+    championship = []
+    winner = []
+
+    bracket_likelihood = 1
+
+    # simulate first round
+    bracket_likelihood = simulate_round_highest_likelihood(first_round, second_round, bracket_likelihood, data)
+    # simulate second round
+    bracket_likelihood = simulate_round_highest_likelihood(second_round, sweet_sixteen, bracket_likelihood, data)
+    # simulate sweet sixteen
+    bracket_likelihood = simulate_round_highest_likelihood(sweet_sixteen, elite_eight, bracket_likelihood, data)
+    # simulate elite eight
+    bracket_likelihood = simulate_round_highest_likelihood(elite_eight, final_four, bracket_likelihood, data)
+    # simulate final four
+    bracket_likelihood = simulate_round_highest_likelihood(final_four, championship, bracket_likelihood, data)
+    # simulate championchip
+    bracket_likelihood = simulate_round_highest_likelihood(championship, winner, bracket_likelihood, data)
+    
+    return bracket_likelihood
+
+def simulate_round_highest_ranked(cur_round, next_round, bracket_likelihood, data):
+    """
+    The function generates a round and returns the
+    likelihood that the higher ranked team wins in 
+    each match wins.
+    """
+    for i in range(0, len(cur_round), 2):
+        # determine higher ranked team
+        higher_ranked1 = first_round_id[i][0]  # makes arbitrary choice of first team if tied
+        lower_ranked1 = first_round_id[i][1]
+        higher_ranked2 = first_round_id[i + 1][0]
+        lower_ranked2 = first_round_id[i + 1][1]
+        if data[higher_ranked1][3] > data[lower_ranked1][3]:
+            lower_ranked1 = higher_ranked1
+            higher_ranked1 = first_round_id[i][1]
+        if data[higher_ranked2][3] > data[lower_ranked2][3]:
+            lower_ranked2 = higher_ranked2
+            higher_ranked2 = first_round_id[i + 1][1]
+
+        # generate probabilites
+        game1_prob = A_beats_B(generate_dist(data[higher_ranked1]), generate_dist(data[lower_ranked1]))
+        game2_prob = A_beats_B(generate_dist(data[higher_ranked2]), generate_dist(data[lower_ranked2]))
+
+        # set up next round
+        next_game = [higher_ranked1, higher_ranked2]
+        next_round.append(next_game)
+
+        # calculate current likelihood
+        bracket_likelihood *= game1_prob / 0.5 * game2_prob / 0.5
+
+    return bracket_likelihood
+
+def highest_ranked(first_round, data):
+    """
+    Given a tournament bracket, the function
+    determines the likelihood of a bracket 
+    with no upsets.
+    """
+    second_round = []
+    sweet_sixteen = []
+    elite_eight = []
+    final_four = []
+    championship = []
+    winner = []
+
+    bracket_likelihood = 1
+
+    # simulate first round
+    bracket_likelihood = simulate_round_highest_ranked(first_round, second_round, bracket_likelihood, data)
+    # simulate second round
+    bracket_likelihood = simulate_round_highest_ranked(second_round, sweet_sixteen, bracket_likelihood, data)
+    # simulate sweet sixteen
+    bracket_likelihood = simulate_round_highest_ranked(sweet_sixteen, elite_eight, bracket_likelihood, data)
+    # simulate elite eight
+    bracket_likelihood = simulate_round_highest_ranked(elite_eight, final_four, bracket_likelihood, data)
+    # simulate final four
+    bracket_likelihood = simulate_round_highest_ranked(final_four, championship, bracket_likelihood, data)
+    # simulate championchip
+    bracket_likelihood = simulate_round_highest_ranked(championship, winner, bracket_likelihood, data)
+    
+    return bracket_likelihood
+
+def simulate_round_random_likelihood(cur_round, next_round, bracket_likelihood, data):
+    """
+    The function generates a round and returns the
+    likelihood a random team wins. 
+    """
+    for i in range(0, len(cur_round), 2):
+        # randomly selct winners
+        winner1 = first_round_id[i][0]  # makes arbitrary choice of first team if tied
+        loser1 = first_round_id[i][1]
+        winner2 = first_round_id[i + 1][0]
+        loser2 = first_round_id[i + 1][1]
+
+        if random.random() >= 0.5:
+            loser1 = winner1
+            winner1 = first_round_id[i][1]
+            loser2 = winner2
+            winner2 = first_round_id[i + 1][1]
+
+        # generate probabilites
+        game1_prob = A_beats_B(generate_dist(data[winner1]), generate_dist(data[loser1]))
+        game2_prob = A_beats_B(generate_dist(data[loser1]), generate_dist(data[loser2]))
+
+        # set up next round
+        next_game = [winner1, winner2]
+        next_round.append(next_game)
+
+        # calculate current likelihood
+        bracket_likelihood *= game1_prob / 0.5 * game2_prob / 0.5
+    return bracket_likelihood
+
+def random_likelihood(first_round, data):
+    """
+    Given a tournament bracket, the function
+    determines the likelihood of a random
+    bracket.
+    """
+    second_round = []
+    sweet_sixteen = []
+    elite_eight = []
+    final_four = []
+    championship = []
+    winner = []
+
+    bracket_likelihood = 1
+
+    # simulate first round
+    bracket_likelihood = simulate_round_random_likelihood(first_round, second_round, bracket_likelihood, data)
+    # simulate second round
+    bracket_likelihood = simulate_round_random_likelihood(second_round, sweet_sixteen, bracket_likelihood, data)
+    # simulate sweet sixteen
+    bracket_likelihood = simulate_round_random_likelihood(sweet_sixteen, elite_eight, bracket_likelihood, data)
+    # simulate elite eight
+    bracket_likelihood = simulate_round_random_likelihood(elite_eight, final_four, bracket_likelihood, data)
+    # simulate final four
+    bracket_likelihood = simulate_round_random_likelihood(final_four, championship, bracket_likelihood, data)
+    # simulate championchip
+    bracket_likelihood = simulate_round_random_likelihood(championship, winner, bracket_likelihood, data)
+    
+    return bracket_likelihood
+
 
 if __name__ == "__main__":
     main()
---
+++ teams.py
@@ -0,0 +1,20 @@
+# file contains list of the matchups in the first round
+# the list is organized where the next round is the winner of each inner list
+first_round = (['Alabama Crimson Tide', 'Texas A&M-Corpus Christi Islanders'], ['Maryland Terrapins', 'West Virginia Mountaineers'], ['San Diego State Aztecs', 'College of Charleston Cougars'], ['Virginia Cavaliers', 'Furman Paladins'],
+               ['Creighton Bluejays', 'North Carolina State Wolfpack'], ['Baylor Bears', 'California-Santa Barbara Gauchos'], ['Missouri Tigers', 'Utah State Aggies'], ['Arizona Wildcats', 'Princeton Tigers'],
+               ['Purdue Boilermakers', 'Fairleigh Dickinson Knights'], ['Memphis Tigers', 'Florida Atlantic Owls'], ['Duke Blue Devils', 'Oral Roberts Golden Eagles'], ['Tennessee Volunteers', 'Louisiana-Lafayette Ragin'],
+               ['Kentucky Wildcats', 'Providence Friars'], ['Kansas State Wildcats', 'Montana State Bobcats'], ['Michigan State Spartans', 'Southern California Trojans'], ['Marquette Golden Eagles', 'Vermont Catamounts'],
+               ['Houston Cougars', 'Northern Kentucky Norse'], ['Iowa Hawkeyes', 'Auburn Tigers'], ['Miami (FL) Hurricanes', 'Drake Bulldogs'], ['Indiana Hoosiers', 'Kent State Golden Flashes'],
+               ['Iowa State Cyclones', 'Pittsburgh Panthers'], ['Xavier Musketeers', 'Kennesaw State Owls'], ['Texas A&M Aggies', 'Penn State Nittany Lions'], ['Texas Longhorns', 'Colgate Raiders'],
+               ['Kansas Jayhawks', 'Howard Bison'], ['Arkansas Razorbacks', 'Illinois Fighting Illini'], ['Saint Mary\'s (CA) Gaels', 'Virginia Commonwealth Rams'], ['Connecticut Huskies', 'Iona Gaels'],
+               ['Texas Christian Horned Frogs', 'Arizona State Sun Devils'], ['Gonzaga Bulldogs', 'Grand Canyon Antelopes'], ['Northwestern Wildcats', 'Boise State Broncos'], ['UCLA Bruins', 'North Carolina-Asheville Bulldogs'])
+
+# this list contains the indexes of the above teams in the data returned by tournament teams
+first_round_id = ([0, 63], [26, 18], [13, 48], [33, 52],
+                  [12, 41], [14, 54], [44, 15], [8, 56],
+                  [7, 67], [20, 19], [23, 42], [3, 53],
+                  [30, 39], [21, 57], [32, 35], [11, 59],
+                  [1, 62], [34, 25], [38, 50], [28, 46],
+                  [22, 51], [16, 60], [27, 40], [6, 58],
+                  [9, 64], [17, 31], [10, 45], [4, 49],
+                  [24, 47], [5, 55], [36, 29], [2, 61])
